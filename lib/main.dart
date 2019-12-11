9fb0cc6f397416538392cd8e5bcdba131a04961e
André Vieira - IVIA <sgavsnake@gmail.com>
2019-12-11 10:50:28 -0300

    add favorites interaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/product_overview_screen.dart';
 import './screens/product_detail_screens.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,36 +11,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'My Shop',
-        theme: ThemeData(
-            fontFamily: 'Lato',
-            primarySwatch: Colors.purple,
-            accentColor: Colors.deepOrange),
-        home: ProductsOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-        });
+    return ChangeNotifierProvider.value(
+          value: Products(),
+          child: MaterialApp(
+          title: 'My Shop',
+          theme: ThemeData(
+              fontFamily: 'Lato',
+              primarySwatch: Colors.purple,
+              accentColor: Colors.deepOrange),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          }),
+    );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Third App - Shop App"),
-//       ),
-//       body: Center(
-//         child: Text("Take a pratice"),
-//       ),
-
-//       // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
